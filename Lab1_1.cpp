47671c49cd4ad1116de2286b631a538a0cae657d
Алексей Порозов <yageroy@mail.ru>
2013-09-19 01:27:26 +0600

    Update Lab1_1.cpp
    commit

--- Lab1_1.cpp
+++ Lab1_1.cpp
@@ -25,12 +25,16 @@ denominator = counter = 0; //#AB0=02;8205< =0G0;L=K5 7=0
 printf( #25;8G8205< AG5BG8: 8 >B>1@0605< =0 M:@0=5\n );
 
 printf( %d\n ,counter++);
+/*    /\
+      ||
+ it's not allowed 
+     			 */
 
 printf( KG8A;O5< 7=0G5=85 Counter/Denominator =  );
 
-printf( %d\n ,counter/denominator);
+printf( %d\n ,counter/denominator); /* division by zero */
 
-printf(counter);
+printf(counter); /* where is the "%d"? */
 
 
 
@@ -40,5 +44,3 @@ printf(counter);
 1) кавычки русские - синтаксис
 2) деление на 0; 31 строка - логическая
 3) отсутствует %d, 33 строка - семантическая
-
-
